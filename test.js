e6bd33b938a96429f7f14ab53b9997d4adf68509
Abe <abesell132@gmail.com>
2021-12-09 14:35:36 -0500

    asdf

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const Jimp = require("jimp");
 
 Jimp.read("./NeuralNetwork/training/triangle-1.png")
   .then(function (image) {
-    console.log(image);
+    console.log(image.getPixelColor.toString());
   })
   .catch(function (err) {
     console.error(err);
