e758104bce762186d60082f22923c90bb10b5cbd
Abe <abesell132@gmail.com>
2021-12-09 10:19:33 -0500

    asf

--- NeuralNetwork/training/index.js
+++ NeuralNetwork/training/index.js
@@ -29,7 +29,7 @@ module.exports = training = {
     trainingTargets = await tf.oneHot(labelTensor, 4);
     await labelTensor.dispose();
 
-    await console.log(trainingInputs);
+    await console.log(trainingInputs[0]);
 
     return new Promise((resolve) => {
       model.fit(trainingInputs, trainingTargets, {
